c822e783cf75af8a0ca8d72e823836b70628ab36
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-12-19 20:39:05 -0600

    spa day validation updated

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,40 @@
+package org.launchcode.controllers;
+
+import jakarta.validation.Valid;
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+
+        if(errors.hasErrors()){
+            return"user/add";
+        }
+
+        if (user.getPassword().equals(verify)) {
+            return "user/index";
+        }
+        else {
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
+        }
+
+    }
+
+
+}
